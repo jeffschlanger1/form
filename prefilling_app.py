963e31c5947d98970aa5ae52585570d393fd595f
jeffschlanger1 <jeff.schlanger@integrassure.com>
2024-12-02 23:32:51 +0500

    2-12-2024 (2)

--- prefilling_app.py
+++ prefilling_app.py
@@ -10,6 +10,7 @@ from datetime import datetime
 # Load environment variables
 load_dotenv()
 api_key=st.secrets["OPENAI_API_KEY"]
+st.write(api_key)
 # Initialize OpenAI client
 # client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 client = OpenAI(api_key=api_key)
